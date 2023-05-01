7701170b370216d06e663a102e914cf53c6cab8a
Gytis Vaicius <gyteniz@gmail.com>
2023-05-01 21:26:56 +0300

    producto atvaizdavimas product.html, views, urls

--- mysite/eshop/urls.py
+++ mysite/eshop/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.products, name='products'),
+    path('products/<int:product_id>', views.product, name='product'),
 
 ]
--- mysite/eshop/views.py
+++ mysite/eshop/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Product
+from django.shortcuts import render, get_object_or_404
+
 # Create your views here.
 def products(request):
     context = {
         'products': Product.objects.all()
     }
     return render(request, 'products.html', context=context)
+
+
+def product(request, product_id):
+    context = {
+    'product': get_object_or_404(Product, pk=product_id)
+    }
+    return render(request, 'product.html', context=context)
