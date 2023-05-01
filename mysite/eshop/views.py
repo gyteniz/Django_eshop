363d5c2051121df635fb93250c980cedc6c7653c
Gytis Vaicius <gyteniz@gmail.com>
2023-05-01 21:01:45 +0300

    frontend atvaizdavimas produktu saraso padarymas products.html

--- mysite/eshop/views.py
+++ mysite/eshop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def products(request):
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'products.html', context=context)
