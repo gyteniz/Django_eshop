4099bbdfb35ba7e5332af95a122f8081161405e2
Gytis Vaicius <gyteniz@gmail.com>
2023-05-01 12:35:36 +0300

    sukurtas requirements.txt failiukas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from my_settings import SECRET_KEY
+from .my_settings import SECRET_KEY
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
